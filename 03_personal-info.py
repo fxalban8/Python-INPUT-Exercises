009f2e39ad90ab31c73393b4b50decb78df30a72
fxalban8 <francisco.alban@epn.edu.ec>
2020-05-25 22:01:28 -0500

    It's been established the sentence with the parameters

---
+++ 03_personal-info.py
@@ -0,0 +1,9 @@
+#Asking for four pieces of information
+first_name=input("Write your first name: ")
+second_name= input("Write your second name: ")
+identity_number= input("Write your ID number: ")
+college= input("Write the institute where you are studying: ")
+space = " "
+
+#Writing a sentence with the parameters above:
+print("Hi "+first_name+space+second_name+space+"your Id # is "+ identity_number+space+"and you belong to "+college)
